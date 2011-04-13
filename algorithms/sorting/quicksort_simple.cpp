162f6eb80a484d6c42e501a8567ec5c77a69fb8d
Daniel Packer <dp@Daniel-Packers-MacBook-Air.local>
2011-04-13 01:42:39 -0400

    Added docs

--- algorithms/sorting/quicksort_simple.cpp
+++ algorithms/sorting/quicksort_simple.cpp
@@ -1,16 +1,8 @@
 /*
 
- function quicksort(array)
-     var list less, greater
-     if length(array) ≤ 1
-         return array  // an array of zero or one elements is already sorted
-     select and remove a pivot value pivot from array
-     for each x in array
-         if x ≤ pivot then append x to less
-         else append x to greater
-     return concatenate(quicksort(less), pivot, quicksort(greater))
+    All code by Daniel Packer.
 
-pseucodecode from wikipedia, requires O(N) extra space. not best approach.
+    This is my really horrifying implementation of in-place quicksort. It uses a naive pivot scheme, but seems to run about as fast as the gnu c++ STL sort() on large vectors of ints so far. Use with caution. Working on better pivoting scheme.
 
 */
 
