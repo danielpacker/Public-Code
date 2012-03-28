4c054e3389cd092895a1215712f81f112ee9ae37
Obscurite <obscurite@obscurite220.(none)>
2012-03-28 02:25:50 -0400

    working checking now for true/false keys

--- algorithms/bioinformatics/biocomputing/xorhash.py
+++ algorithms/bioinformatics/biocomputing/xorhash.py
@@ -3,17 +3,17 @@ import collections
 
 
 # Run an xor chain on a given binary string with a given boolean key
-def xor_chain(abyte="00000000", len=8, key=False):
+def xor_chain(abyte="00000000", key=False, len=8):
 	bytelist = list(abyte[::-1]) # reverse string for right->left
 	hash = list()
 	for i in range(len):
-		print(bytelist[i])
+		#print(bytelist[i])
 		bit = bool(int(bytelist[i]))
-		print("bit: " + str(bit))
+		#print("bit: " + str(bit))
 		key = bit ^ key
-		print("key: " + str(key))
+		#print("key: " + str(key))
 		hash.append(str(int(key)))
-	print("\n")
+	#print("\n")
 	return "".join(hash)
 
 # convert byte (int from 0..255) to binary string
@@ -27,11 +27,16 @@ def byte_to_bin(rbyte=-1):
 
 # make sure function is 1:1 for all bytes
 keyvals = {}
-for i in range(255):
-	bstr = byte_to_bin(i)
-	keyvals[bstr] = xor_chain(bstr)
-c = collections.Counter(keyvals.values())
-#print(keyvals)
-numduplicates = len([i for i in c if c[i]>1])
-numoriginals = len(keyvals.values())
-print("number of originals: " + str(numoriginals) + " number of duplicates: " + str(numduplicates) + "\n")
+for b in [False, True]:
+	#print("Testing for key=" + str(b) + "\n")
+	for i in range(255):
+		bstr = byte_to_bin(i)
+		keyvals[bstr] = xor_chain(bstr, b)
+	c = collections.Counter(keyvals.values())
+	#print(keyvals)
+	duplicates = [i for i in c if c[i]>1]
+	numduplicates = len(duplicates)
+	if (numduplicates):
+		print("DUPLICATES FOUND: " + str(duplicates) + "\n")
+	numoriginals = len(keyvals.values())
+	print("number of originals: " + str(numoriginals) + " number of duplicates: " + str(numduplicates) + "\n")
