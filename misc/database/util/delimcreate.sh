4c17f618d8747916939ea97c4bb315cbf0faa3be
Obscurite <obscurite@obscurite220.(none)>
2013-03-12 22:45:19 -0400

    added semilcolons for mysql

--- misc/database/util/delimcreate.sh
+++ misc/database/util/delimcreate.sh
@@ -6,10 +6,10 @@
 TABLE=$1
 FILE=$2
 DELIM=$3
-echo "DROP TABLE IF EXISTS $TABLE"
+echo "DROP TABLE IF EXISTS $TABLE;"
 echo -n "CREATE TABLE $TABLE ( "
 head -1 $FILE | perl -n -e "chomp; print join(qq/ varchar(255), /, split(/$DELIM/)); print qq/ varchar(255)/;"
 echo -n $RESULT
-echo " )"
+echo " );"
 
 
