dd7bca494c4e69891bdde350c1acbe66934f87df
Obscurite <obscurite@obscurite220.(none)>
2013-03-28 03:12:31 -0400

    added size/seq distributions to fasta stats

--- algorithms/bioinformatics/formats/fasta_utils.pm
+++ algorithms/bioinformatics/formats/fasta_utils.pm
@@ -10,27 +10,38 @@ use Bio::SeqIO;
 use constant DEFAULT_MAXSEQS => 10*1000;
 use constant DEFAULT_GUESS_CHARS => 10000*1024;
 
-sub fasta_stats($;$)
+# return number of sequences, average seq length, and total length of all seqs
+sub fasta_stats
 {
-  my $filename = shift or die "no filename";
-  my $maxseqs = shift;
-  $maxseqs = defined($maxseqs) ? $maxseqs : DEFAULT_MAXSEQS;
+  my %args = @_;
+  my $filename = $args{'filename'} or die "no filename";
+  my $maxseqs = $args{'maxseqs'};
+  $maxseqs = defined($args{'maxseqs'}) ? $args{'maxseqs'} : DEFAULT_MAXSEQS;
+  my $do_freq_dist = $args{'freq_dist'} || 0;
+
+  my (%freq_dist, %size_dist);
 
   my $in = new Bio::SeqIO(-file => $filename, -format => 'fasta');
   my $count = 1;
   my $len = 0;
   while(my $seq = $in->next_seq) {
     $len += $seq->length();
+    $size_dist{$seq->length()}++;
+    $freq_dist{$seq->display_id()}++ if ($do_freq_dist); # gather freq dist if needed
     last if ($count == $maxseqs);
     $count++;
   }
-  return (
+  my %ret = (
     'count' => $count, 
     'avg_length' => $len/$count, 
-    'total_length' => $len
+    'total_length' => $len,
+    'size_dist' => {%size_dist}
   );
+  %ret = (%ret, 'freq_dist' => {%freq_dist}) if ($do_freq_dist);
+  return %ret;
 }
 
+# read a fixed number of bytes of the fasta file and guess number of sequences
 sub guess_num_seqs($)
 {
   my $filename = shift or die "no filename";
