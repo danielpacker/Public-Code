43c46f68a07d96298f9d4c168ace3bf1460e87dd
Obscurite <obscurite@obscurite220.(none)>
2013-08-13 14:12:04 -0400

    updated sman with verbose mode

--- algorithms/bioinformatics/sman.pl
+++ algorithms/bioinformatics/sman.pl
@@ -1,9 +1,18 @@
-#!/usr/bin/perl -w 
+#!/usr/bin/perl -w
+#
+# Daniel Packer <dp@danielpcaker.org>
+#
+# sman.pl is a simple command line utility for
+# quick and basic DNA/RNA/protein sequence manipulations
 
 use strict;
 use warnings;
 
 use Bio::Perl;
+use Getopt::Long;
+
+my $verbose = 0;
+my $result = GetOptions ("verbose"  => \$verbose, "v" => \$verbose);  # flags
 
 # do operations on sequences
 my $usage = "Usage: sman.pl OPERATION SEQUENCES...";
@@ -28,9 +37,31 @@ sub do_op {
   }
   elsif ($op =~ /^com/i) # complement
   {
-    #my $s2 = $seq; $s2 =~ tr/actgACTG/tgacTGAC/;
     my $s2 = $seq; $s2 =~ tr/AaCcGgTt/TtGgCcAa/;
     return $s2;
+  }
+  elsif ($op =~ /^tran/i) # translate
+  {
+    my $seqobj = Bio::Seq->new(-seq => $seq, -alphabet => 'dna' );
+    my $prots_str = "";
+    for my $frame (1..3) {
+      my $prot = $seqobj->translate(-frame => $frame-1, -start=>"ATG");
+      my $prot_str;
+
+      if ($verbose) # in versbose mode show codon alignment, original seq
+      {
+        my @aas = split("", $prot->seq());
+        $prot_str = "  $seq\n" . "$frame " . "-" x ($frame-1) . 
+          join("", map { "[$_]"} @aas) . "\n\n";
+      }
+      else # basic info
+      {
+        $prot_str = "$frame " . $prot->seq() . "\n";
+      }
+      $prots_str .= $prot_str;
+
+    }
+    return $prots_str;
   }
 }
 
